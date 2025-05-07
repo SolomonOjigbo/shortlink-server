4d17fcfeb84c88dbfc8dcbb345c8491a20d3a802
‘Solomon <solomonojigbo@gmail.com>
2025-05-07 06:13:48 +0100

    Implement whitelist all origins in corsoptions

--- index.js
+++ index.js
@@ -10,28 +10,15 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      
-      // List of allowed domains
-      const allowedDomains = [
-        'http://localhost:3000', 
-        'https://shortlink-ecru.vercel.app/'
-      ];
-      
-      if (allowedDomains.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*', 
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: false 
   };
   
-  // Middleware
-  app.use(cors(corsOptions));
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(express.json());
 
 app.use('/', urlRoutes);
